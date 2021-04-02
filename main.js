74343a000b8c6cdba92bdea86751444fd5e2399d
VD25 <76209392+VD25@users.noreply.github.com>
2021-04-02 15:32:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("hello")
+console.log("My first console project")
+var firstname="Vihaan"
+var lastname="Dhameja"
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+var x=10;
+var y=6;
+var add=x+y;
+var subtract=x-y;
+var multiply=x*y;
+var divide=x/y;
+console.log(add);
+console.log(subtract);
+console.log(multiply);
+console.log(divide);
